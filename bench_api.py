9d5f07b11bbdb45e2f82ef881cfa0d52aa04686d
daipayan <dbanerjee13@gmail.com>
2020-08-10 12:17:41 -0400

    No functional changes, just lint cleaned.

--- bench_api.py
+++ bench_api.py
@@ -1,54 +1,60 @@
-from kubernetes import client, config
-import yaml
-from os import path, mkdir
-import time
+"""A python script automating kube-bench worker node scans using the Kubernetes\
+    Pythcon Client API"""
 import datetime
 import shutil
+from time import sleep
+from os import path, mkdir
+import yaml
+from kubernetes import client, config
 
 # api = client.CoreV1Api()
 # nodes = api.list_node()
 def apply_yaml():
-    #Kubectl apply the kube-bench DaemonSet .yaml file
+    """Kubectl apply the kube-bench DaemonSet .yaml file"""
     config.load_kube_config()
-    with open(path.join(path.dirname(__file__), "job-node_DaemonSet.yaml")) as f:
-        #Load the yaml file 
-        daemon = yaml.safe_load(f)
-        apply_resp = client.AppsV1Api().create_namespaced_daemon_set(body=daemon, namespace="default")
+    with open(path.join(path.dirname(__file__), "job-node_DaemonSet.yaml")) as yaml_file:
+        #Load the yaml file
+        daemon = yaml.safe_load(yaml_file)
+        apply_resp = client.AppsV1Api().create_namespaced_daemon_set(body=daemon, \
+            namespace="default")
         print("DaemonSet created. status='%s'" % apply_resp.metadata.name)
         return apply_resp.metadata.name
 
 def delete_apply(pods_name):
-    #Delete the kube-bench DaemonSet
-    delete_resp = client.AppsV1Api().delete_namespaced_daemon_set(name=pods_name, namespace="default")
+    """Delete the kube-bench DaemonSet"""
+    client.AppsV1Api().delete_namespaced_daemon_set(name=pods_name, \
+        namespace="default")
 
 def delete_old():
-    #Delete results of previous scans
-    if path.exists('outputs') == True:
+    """Delete results of previous scans"""
+    if path.exists('outputs'):
         shutil.rmtree('outputs')
     mkdir('outputs')
 
-def write_logs(pod_name, node_name, logs):
-    #Write the ouput logs to files named Node wise 
-    with open("outputs/"+ node_name,"a") as f:
+def write_logs(node_name, logs):
+    """Write the ouput logs to files named Node wise"""
+    with open("outputs/"+ node_name, "a") as log_file:
         #Add time and date of scan to the beginning of the ouput file
-        f.write(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")+"\n")
-        f.write(logs)
+        log_file.write(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")+"\n")
+        log_file.write(logs)
 
 def get_logs(pods_name):
-    #Fetch logs for the kub-bench pods
+    """Fetch logs for the kub-bench pods"""
     ret = client.CoreV1Api().list_pod_for_all_namespaces(watch=False)
-    pods = []
     for i in ret.items:
-        #fetch logs of pods with pods_name = "kube-bench-node-*" 
-        if( pods_name in i.metadata.name):
+        #fetch logs of pods with pods_name = "kube-bench-node-*"
+        if pods_name in i.metadata.name:
             print(i.metadata.name, i.spec.node_name)
-            logs_response = client.CoreV1Api().read_namespaced_pod_log(name=i.metadata.name, namespace="default")
-            #Write logs to the files 
-            write_logs(i.metadata.name, i.spec.node_name, logs_response)
+            logs_response = client.CoreV1Api().read_namespaced_pod_log(\
+            name=i.metadata.name, namespace="default")
+            #Write logs to the files
+            write_logs(i.spec.node_name, logs_response)
 
 def main():
+    """Main Function"""
     pods_name = apply_yaml()
-    time.sleep(60) 
+    print(get_logs.__doc__)
+    sleep(60)
     delete_old()
     get_logs(pods_name)
     delete_apply(pods_name)
