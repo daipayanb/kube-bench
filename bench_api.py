5bfce1d33809165b27d4ccc32434461a5b541b95
daipayan <dbanerjee13@gmail.com>
2020-08-11 18:41:39 -0400

    Ouput now in JSON format, Checks on Container status and Retry Block

--- bench_api.py
+++ bench_api.py
@@ -1,7 +1,8 @@
 """A python script automating kube-bench worker node scans using the Kubernetes\
     Pythcon Client API"""
-import datetime
 import shutil
+import random
+import string
 from time import sleep
 from os import path, mkdir
 import yaml
@@ -12,12 +13,19 @@ from kubernetes import client, config
 def apply_yaml():
     """Kubectl apply the kube-bench DaemonSet .yaml file"""
     config.load_kube_config()
+    #Unique identifier for specific kube-bench runs
+    unq = ''.join(random.choices(string.ascii_lowercase + string.digits, k=16))
     with open(path.join(path.dirname(__file__), "job-node_DaemonSet.yaml")) as yaml_file:
         #Load the yaml file
         daemon = yaml.safe_load(yaml_file)
+        #Editing the load yaml file with the unique names
+        kube_bench_node_name = daemon['metadata']['name'] + "-" + unq
+        daemon['metadata']['name'] = kube_bench_node_name
+        daemon['spec']['selector']['matchLabels']['name'] = kube_bench_node_name
+        daemon['spec']['template']['metadata']['labels']['name'] = kube_bench_node_name
         apply_resp = client.AppsV1Api().create_namespaced_daemon_set(body=daemon, \
             namespace="default")
-        print("DaemonSet created. status='%s'" % apply_resp.metadata.name)
+        print("DaemonSet created. Daemon-Set name='%s'" % apply_resp.metadata.name)
         return apply_resp.metadata.name
 
 def delete_apply(pods_name):
@@ -33,30 +41,83 @@ def delete_old():
 
 def write_logs(node_name, logs):
     """Write the ouput logs to files named Node wise"""
-    with open("outputs/"+ node_name, "a") as log_file:
-        #Add time and date of scan to the beginning of the ouput file
-        log_file.write(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")+"\n")
+    with open("outputs/"+ node_name +".json", "a") as log_file:
         log_file.write(logs)
+
+def node_pod(pod_name):
+    """Get Node in which a pod is running"""
+    ret = client.CoreV1Api().list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        if pod_name == i.metadata.name:
+            return i.spec.node_name
+    return pod_name
+
+def get_pod_list(pods_name):
+    """Returns a dictionary with keys= Pod names and value= whether logs of pods are available"""
+    ret = client.CoreV1Api().list_pod_for_all_namespaces(watch=False)
+    pod_log_status = {}
+    for i in ret.items:
+        if pods_name in i.metadata.name:
+            pod_log_status[i.metadata.name] = "LogsNotChecked"
+    #print(pod_log_status)
+    return pod_log_status
+
+def check_logs_ready(pod_log_status, pods_name):
+    """Verify if logs are ready to be fetched based on the\
+         status of the pods, restarts and contents of the logs."""
+    print(pod_log_status)
+    retries = 5
+    time = 5
+    while retries > 0:
+        #Flag updated to True if any changes are made to pod_log_status\
+        # if not flag stays false and the retry block is exitted.
+        update = False
+        sleep(time)
+        ret = client.CoreV1Api().list_pod_for_all_namespaces(watch=False)
+        for i in ret.items:
+            if pods_name in i.metadata.name and pod_log_status[i.metadata.name] != "LogsReady":
+                try:
+                    if i.status.container_statuses[0].state.waiting.reason == 'CrashLoopBackOff'\
+                        or i.status.container_statuses[0].state.waiting.reason == 'Completed'\
+                            or i.status.container_statuses[0].restart_count >= 1:
+                        pod_log_status[i.metadata.name] = "LogsReady"
+                        #print(i.metadata.name, i.status, i.status.container_statuses[0].\
+                        # state.waiting.reason)
+                        update = True
+                except ContainerError:
+                    pod_log_status[i.metadata.name] = "LogsNotReady"
+                    print(i.metadata.name + " Kube-bench container in unusable state")
+                    update = True
+        if not update: #If no updates made
+            break
+        retries -= 1
+        time *= 2 #Sleep time before each re-attempt is doubled
+    for pods in pod_log_status: #Print the pods which didin't reach a usuable state
+        if pod_log_status[pods] != "LogsReady":
+            print("Retried 5 times. Logs from POD: %s are still not available. \
+                Please check manually" % pods)
+    return pod_log_status
 
 def get_logs(pods_name):
     """Fetch logs for the kub-bench pods"""
-    ret = client.CoreV1Api().list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        #fetch logs of pods with pods_name = "kube-bench-node-*"
-        if pods_name in i.metadata.name:
-            print(i.metadata.name, i.spec.node_name)
+    pod_log_status = get_pod_list(pods_name)
+    pod_log_status = check_logs_ready(pod_log_status, pods_name)
+    for pod in pod_log_status:
+        #print(pod + " " + pod_log_status[pod])
+        if pod_log_status[pod] == "LogsReady":
             logs_response = client.CoreV1Api().read_namespaced_pod_log(\
-            name=i.metadata.name, namespace="default")
+            name=pod, namespace="default")
             #Write logs to the files
-            write_logs(i.spec.node_name, logs_response)
+            node_name = node_pod(pod)
+            write_logs(node_name, logs_response)
 
 def main():
     """Main Function"""
     pods_name = apply_yaml()
-    print(get_logs.__doc__)
-    sleep(60)
+    sleep(5)
     delete_old()
     get_logs(pods_name)
+    sleep(30)
     delete_apply(pods_name)
 
 if __name__ == "__main__":
